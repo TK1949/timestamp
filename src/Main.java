20ea90b3f9755c932b13cba5c67e253f24e8fe47
LeungZengJian <859806649@qq.com>
2019-09-06 19:41:25 +0800

    优化代码

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(TimeTool.single().now());
+        System.out.println(TimeTool.currentTimeMillis());
+        System.out.println(TimeTool.cacheTimeMillis());
     }
 }
--- src/TimeTool.java
+++ src/TimeTool.java
@@ -3,31 +3,37 @@ import java.util.concurrent.TimeUnit;
 
 public class TimeTool {
 
-    private static class Instance {
-        private static final TimeTool single = new TimeTool();
-    }
-
-    public static TimeTool single() {
-        return Instance.single;
-    }
-
-    private volatile long now;
-
     private TimeTool() {
-        now = System.currentTimeMillis();
-        Runnable task = () -> now = System.currentTimeMillis();
-        new ScheduledThreadPoolExecutor(1, r -> {
-            Thread thread = new Thread("current-time-millis");
-            thread.setDaemon(true);
-            return thread;
-        }).scheduleAtFixedRate(task, 1, 1, TimeUnit.MILLISECONDS);
     }
 
-    public long now() {
-        return now;
+    /**
+     * 获取缓存时间戳
+     */
+    public static long cacheTimeMillis() {
+        return CacheTime.single.now;
     }
 
+    /**
+     * 直接获取系统时间戳
+     */
     public static long currentTimeMillis() {
         return System.currentTimeMillis();
+    }
+
+    private static class CacheTime {
+
+        private static final CacheTime single = new CacheTime();
+
+        private volatile long now;
+
+        private CacheTime() {
+            now = System.currentTimeMillis();
+            Runnable task = () -> now = System.currentTimeMillis();
+            new ScheduledThreadPoolExecutor(1, r -> {
+                Thread thread = new Thread("cache-time-millis");
+                thread.setDaemon(true);
+                return thread;
+            }).scheduleAtFixedRate(task, 1, 1, TimeUnit.MILLISECONDS);
+        }
     }
 }
